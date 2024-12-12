b08d26a226e5ea6a51fc313a06e392c9868267bd
Isaac-Tellezs <andreescalante34@gmail.com>
2024-12-11 18:23:01 -0600

    add vercel configuration

--- src/server.js
+++ src/server.js
@@ -22,6 +22,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: 'https://trade-nationn.netlify.app/',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(UserRoutes);
 app.use(StripeRoutes);
