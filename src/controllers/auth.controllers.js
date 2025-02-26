9df1f1e1299dae9e5e9b5bea89207b337edf1a0e
Isaac-Tellezs <andreescalante34@gmail.com>
2025-02-26 15:36:56 -0600

    delete depuration log

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -116,7 +116,6 @@ export const profile = async (req, res) => {
 export const verifyToken = async (req, res) => {
   const token = req.headers.authorization?.split(" ")[1]; // Recupera el token del encabezado
 
-  // console.log("Token recibido en verifyToken:", token); // Log para depuración
 
   if (!token) {
       return res.status(401).json({ message: "No token, authorization denied" });
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken';
 export const authRequired = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1]; // Recupera el token del encabezado
 
-    console.log("Token recibido en authRequired:", token); // Log para depuración
 
     if (!token) {
         console.log("No se encontró token, denegando acceso..."); // Log si no hay token
